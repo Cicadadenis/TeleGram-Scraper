85b09fbca047a6f4a178818c1e2a44cb2e02794c
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-04-09 00:36:56 +0300

    Add files via upload

---
+++ ciada_server.sh
@@ -0,0 +1,13 @@
+wget https://raw.githubusercontent.com/Cicadadenis/TeleGram-Scraper/master/3301_v4.py
+
+wget https://raw.githubusercontent.com/Cicadadenis/TeleGram-Scraper/master/API.txt
+
+wget https://raw.githubusercontent.com/Cicadadenis/TeleGram-Scraper/master/hash.txt
+
+wget https://raw.githubusercontent.com/Cicadadenis/TeleGram-Scraper/master/members.csv
+
+wget https://raw.githubusercontent.com/Cicadadenis/TeleGram-Scraper/master/members2.csv
+
+wget https://raw.githubusercontent.com/Cicadadenis/TeleGram-Scraper/master/reklama.txt
+
+wget https://raw.githubusercontent.com/Cicadadenis/TeleGram-Scraper/master/tel.txt
